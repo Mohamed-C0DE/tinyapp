7f129d49c960022e786aeba7074a74334cad2ff0
Mohamed Ali <12maali27@gmail.com>
2021-12-07 17:30:14 +0000

    created new route where once we search input the short url in the address bar it is redirected to the longURL, also added commenting to code

--- express_server.js
+++ express_server.js
@@ -7,14 +7,12 @@ function generateRandomString() {
   let text = "";
   const possible = "abcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i <= 5; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
 
   return text;
 }
-
-console.log(generateRandomString());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -25,19 +23,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+// Response sent to homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Response with urls_index ejs file
 app.get("/urls", (req, res) => {
   const templeVars = { urls: urlDatabase };
   res.render("urls_index", templeVars);
 });
 
+// Response with urls_new ejs file
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Response with urls_show ejs file
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -46,9 +48,23 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// Redirects to longURL based on the shortURL key in urlDatabase
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  // Added below if shortURL doesnt exist
+  if (!longURL) {
+    res.send("We dont have any record of that URL");
+  }
+  res.redirect(longURL);
+});
+
+// Generates a random string and references the longURL inputed as the shortURL, then redirects to the urls_show ejs file
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let longURL = req.body.longURL;
+  let shortURL;
+  shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.listen(PORT, () => {
