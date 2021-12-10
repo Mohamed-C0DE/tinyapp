855bfa8a0566f9505b5765866c7768a37eff71d0
Mohamed Ali <12maali27@gmail.com>
2021-12-10 04:02:20 +0000

    moved more helper functions from express_server.js file

--- helpers.js
+++ helpers.js
@@ -6,4 +6,25 @@ const getUserByEmail = (email, database) => {
   }
 };
 
-module.exports = { getUserByEmail };
+const generateRandomString = () => {
+  let text = "";
+  const possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i <= 5; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+};
+
+const urlsForUser = (id, database) => {
+  const urls = {};
+  for (const key in database) {
+    if (database[key].userID === id) {
+      urls[key] = database[key];
+    }
+  }
+  return urls;
+};
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
